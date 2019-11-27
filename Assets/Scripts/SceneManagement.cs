1e70fe83f6177c3b09b556ec1a1156fde83ef525
annamari12 <education.marianna@gmail.com>
2019-11-27 11:29:29 +0200

    Menu UI

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public List<GameObject> button_list = new List<GameObject>();
+    public List<string> load = new List<string>();
+
+    public Image black;
+    public Animator anim;
+
+    void Start()
+    {
+
+
+        foreach (GameObject go in button_list)
+        {
+            int index = button_list.IndexOf(go);
+            go.GetComponent<Button>().onClick.AddListener(() => StartCoroutine(ChangeScene(load[index], SceneManager.GetActiveScene().ToString()))); //Don't touch! :)
+
+        }
+
+
+    }
+
+    private IEnumerator ChangeScene(string toLoad, string toUnload)
+    {
+        anim.SetBool("Fade", true);
+        //Debug.Log("Starting Fade");
+        yield return new WaitUntil(() => black.color.a == 1);
+
+        yield return SceneManager.LoadSceneAsync(toLoad);
+        yield return SceneManager.UnloadSceneAsync(toUnload);
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    public int generalHappiness = 0;
+    public int income = 0;
+    public int workersCount = 0;
+
+    int tempHappiness;
+    
+
+    public List<Worker> workers = new List<Worker>();
+
+    //[System.Serializable]
+    
+
+    [System.Serializable]
+    public class TypesList
+    {
+        public enum Qualifications
+        {
+            Junior = 0,
+            Middle = 1,
+            Senior = 2
+        }
+
+        public static readonly string[] qualifications = { "Junior", "Middle", "Senior" };
+    };
+    
+
+    [System.Serializable]
+    public class Worker
+    {
+        public string name;
+        public TypesList.Qualifications qualification;
+        public int happiness;
+        public int salary;
+        public int companyIncome;
+        
+
+
+        public Worker(string _name, string _qualification, int _happiness, int _salary, int _companyIncome)
+        {
+            name = _name;
+            TypesList.qualifications[(int)qualification] = _qualification;
+            happiness = _happiness;
+            salary = _salary;
+            companyIncome = _companyIncome;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        workersCount = workers.Count;
+
+
+        foreach(Worker worker in workers)
+        {
+            tempHappiness += worker.happiness;
+            income += worker.companyIncome;
+        }
+
+        generalHappiness = tempHappiness / workers.Count;
+        
+
+        /*workers.Add(new Worker("Anton",30, 40));
+        workers.Add(new Worker("Zarina",30, 40));
+        workers.Add(new Worker("Judy",30, 40));*/
+
+        foreach(Worker worker in workers)
+        {
+            Debug.Log("Name: " + worker.name + ";\nQualification: " + worker.qualification + 
+                ";\nHappiness: " + worker.happiness + ";\nSalary (USD/hour) " + worker.salary + ";\nIncome for company: " + worker.companyIncome + "\n");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
+
+
